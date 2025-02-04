7163eb3851bd2c8c07afc99b719f564ebb454be7
Egorkridge <143885524+Egorkridge@users.noreply.github.com>
2025-02-05 00:42:18 +0300

    v3-version.sql

--- v3-version.sql
+++ v3-version.sql
@@ -1,3 +1,31 @@
-/**
-  @author
- */
+alter table Reader
+    add column registrationDate timestamp default current_timestamp;
+alter table Reader
+    add constraint unique_email unique (email);
+alter table Reader
+    alter column age type numeric(10, 1) using age::numeric(10, 1);
+alter table Reader
+    alter column lastName type integer using case when lastname = 'Сидоров' then 1 when lastname = 'Кузнецова' then 2 end;
+
+
+alter table Reader
+    drop column registrationDate;
+alter table Reader
+    drop constraint unique_email;
+alter table Reader
+    alter column age type integer using age::integer;
+alter table Reader
+    alter column lastName type varchar(50) using case when lastname = 1 then 'Сидоров' when lastname = 2 then 'Кузнецова' end;
+
+
+alter table Library
+    add column parentId integer,
+    add constraint parentId foreign key (parentId) references Library (libraryID);
+
+INSERT INTO Library(name, numberOfFloors, parentId)
+VALUES ('Филиал Центральной библиотеки', 2, 1);
+
+INSERT INTO Library(name, numberOfFloors, parentId)
+VALUES ('Филиал Городской библиотеки', 3, 2),
+       ('Детская библиотека', 1, 4);
+ALTER TABLE Book_issue ADD CONSTRAINT unique_issue UNIQUE (reader, book, library);
